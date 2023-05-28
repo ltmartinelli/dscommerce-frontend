452c67a7faa3ce934ab346f243e40564fd04be36
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-05-28 12:42:21 -0300

    Props

--- src/routes/ProductDetails/index.tsx
+++ src/routes/ProductDetails/index.tsx
@@ -3,6 +3,26 @@ import ButtonInverse from "../../components/ButtonInverse";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import HeaderClient from "../../components/HeaderClient";
 import ProductDetailsCard from "../../components/ProductDetailsCard";
+import { ProductDTO } from '../../models/Product';
+
+const product: ProductDTO = {
+    id: 2,
+    name: "TV",
+    description: "Top",
+    imgUrl: "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg",
+    price: 2500.99,
+    categories: [
+        {
+            id: 2,
+            name: "Eletrônicos"
+        },
+        {
+            id: 3,
+            name: "Computadores"
+        }
+    ]
+};
+
 
 export default function ProductDetails()
 {
@@ -11,7 +31,7 @@ export default function ProductDetails()
             <HeaderClient />
             <main>
                 <section id="product-details-section" className="dsc-container">
-                    <ProductDetailsCard />
+                    <ProductDetailsCard product={product} />
                     <div className="dsc-btn-page-container">
                         <ButtonPrimary />
                         <ButtonInverse />
