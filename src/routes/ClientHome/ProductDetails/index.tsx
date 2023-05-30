d4e4b3a2083a7c40d7a4cafc5763f104f9ecff22
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-05-30 17:33:56 -0300

    Requisition Error Handling

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,7 +2,7 @@ import './styles.css';
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { ProductDTO } from '../../../models/product.ts';
@@ -14,14 +14,21 @@ export default function ProductDetails()
 
     const params = useParams();
 
+    const navigate = useNavigate();
+
     const [product, setProduct] = useState<ProductDTO>();
 
     useEffect(() =>
     {
-        productService.findById(Number(params.productId)).then(response =>
-        {
-            setProduct(response.data);
-        })
+        productService.findById(Number(params.productId))
+            .then(response =>
+            {
+                setProduct(response.data);
+            })
+            .catch(() =>
+            {
+                navigate("/");
+            });
     }, []);
 
     return (
