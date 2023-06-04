ecc570d90efdd1f5a3d07bc4ea6e37748db580b4
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-06-04 15:30:29 -0300

    Forms toValues

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -2,3 +2,15 @@ export function update(inputs: any, name: string, newValue: any)
 {
     return { ...inputs, [name]: { ...inputs[name], value: newValue } }
 }
+
+export function toValues(inputs: any)
+{
+    const data : any = {};
+
+    for (const name in inputs)
+    {
+        data[name] = inputs[name].value;
+    }
+
+    return data;
+}
