a184759fb805ebda240f0a533ae394fd867c8c2b
ltmartinelli <lucastrindademartinelli@gmail.com>
2023-06-05 19:48:41 -0300

    Deploy Ready

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+window.React = React
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 )
